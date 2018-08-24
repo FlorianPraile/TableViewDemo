e3e1c359bb78349035be05c23cf76e9a977e64ce
Florian Praile <praile.florian@gmail.com>
2018-08-24 14:17:10 +0200

    TableViewDemo

--- TableViewDemo/ViewController.swift
+++ TableViewDemo/ViewController.swift
@@ -8,11 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var values = ["Hello", "world", "123", "nzlhf"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.tableView.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +25,26 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    //MARK:- TableView Data Source
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return values.count
+    }
 
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let elt = values[indexPath.row]
+        let cell = self.tableView.dequeueReusableCell(withIdentifier: "CellDemo", for: indexPath)
+        cell.textLabel?.text = elt
+        return cell
+    }
+    
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "Hello"
+    }
 }
 
